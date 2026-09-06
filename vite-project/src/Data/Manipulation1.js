06bb5be12700913a69a888e696914fb5b8b59dad
mcagovind980 <kg878672@gmail.com>
2026-09-06 13:35:01 +0530

    react

--- vite-project/src/Data/Manipulation1.js
+++ vite-project/src/Data/Manipulation1.js
@@ -1,75 +1,115 @@
 const books=[
   {
         id:1,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
+        title:"Brandwashed_ Tricks Companies Use to Manipulate ",
+        image:"/public/Images/Manipulation/download.jpg",
         viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
     }
     ,{
         id:2,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"How_To_Analyze_People_13_Laws_About_the_Manipulation_of_the_Human",
+        image:"/public/Images/Manipulation/OIP1.jpg",
+        viewLink:"https://drive.google.com/file/d/1m0OOwSsaX89CmAKV4XPPQWKyXmPbaZ3r/view?usp=drive_link"
     }
     ,{
         id:3,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Introduction To Mentalism_ 7 Mentalism Tricks to Manipulate the Mind",
+        image:"/public/Images/Manipulation/download1.jpg",
+        viewLink:"https://drive.google.com/file/d/1ShaSwJBm7aK8ghUzA7GcdDA0ubYAand9/view?usp=drive_link"
     }
     ,{
         id:4,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Manipulation_ A Guide to Mind Control Tech - Deborah Weiss",
+        image:"/public/Images/Manipulation/download3.jpg",
+        viewLink:"https://drive.google.com/file/d/1lKssiVcq9nZRPQMyRZEnRM0dxdUNiDZa/view?usp=drive_link"
     }
     ,{
         id:5,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Manipulation_ How to Recognize and Outwit Emoticons",
+        image:"/public/Images/Manipulation/OIP2.jpg",
+        viewLink:"https://drive.google.com/file/d/1suTy4pJmM0JWhVI2YC_rpTHE2eTLqh-_/view?usp=drive_link"
     }
     ,{
         id:6,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Manipulation_ How to Recognize and Outwit Emoticons",
+        image:"/public/Images/Manipulation/OIP2.jpg",
+        viewLink:"https://drive.google.com/file/d/1BqzxqHiE7daZqf0U7koGfo3E6Phbmyk5/view?usp=drive_link"
     }
-    ,{
+
+  ,{
         id:7,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Mind Control_ Manipulation, Deception And Persuasion Exposed ",
+        image:"/public/Images/Manipulation/download4.jpg",
+        viewLink:"https://drive.google.com/file/d/1pExYgVRiMNkUOqWhElhkBbXhnkEtXLbN/view?usp=drive_link"
     }
     ,{
         id:8,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Mind_Power__3_Books_in_1__Manipulation_Ho_-_Nadine_Watson",
+        image:"/public/Images/Manipulation/download6.jpg",
+        viewLink:"https://drive.google.com/file/d/1L8W2ua4_ZRSHHKoysXQjlGI2HONaAYUM/view?usp=drive_link"
     }
     ,{
         id:9,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Mind-Sword_ Mastering the Asian Dark Arts",
+        image:"/public/Images/Manipulation/OIP7.jpg",
+        viewLink:"https://drive.google.com/file/d/14q1gf-gzHsipLXwypZ1ay-zgV_pW9_f6/view?usp=drive_link"
     }
-    ,{
+    ,
+    {
         id:10,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Persuasion_ Learn Techniques in Manipulation",
+        image:"/public/Images/Manipulation/download12.jpg",
+        viewLink:"https://drive.google.com/file/d/1tqNJ-7aAayTiAAnaXshDc25d-TR0Cxly/view?usp=drive_link"
     }
-    ,{
+    ,
+
+    {
         id:11,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"Storytelling_Manipulation_of_the_Audience",
+        image:"/public/Images/Manipulation/download13.jpg",
+        viewLink:"https://drive.google.com/file/d/1sLwDkQKqtkx1BHikgzYoDb1U9kdtA5G8/view?usp=drive_link"
     }
-    ,{
+
+    ,
+    
+    {
+    
         id:12,
-        title:"Manipulate Our Minds ",
-        image:"/public/Images/Manipulation/OIP.jpg",
-        viewLink:"https://drive.google.com/file/d/105mB9J6FD1a_rNo-Pgx5cmvwUGRMXac8/view?usp=drive_link"
+        title:"The Art Of Manipulation - R B Sparkman",
+        image:"/public/Images/Manipulation/download14.jpg",
+        viewLink:"https://drive.google.com/file/d/1AiHTs8nuzPdHr8cYAkmV8XtOJk0vLAaL/view?usp=drive_link"
+    }
+,
+    {
+    
+        id:13,
+        title:"The Art Of Manipulation - R B Sparkman",
+        image:"/public/Images/Manipulation/OIP13.jpg",
+        viewLink:"https://drive.google.com/file/d/1EekTruHrsNEPN5UennLgV8DIR2FVmeWd/view?usp=drive_link"
+    }
+,
+    {
+    
+        id:14,
+        title:"The_Nice_Girl_Syndrome_Stop_Being_Manipulated",
+        image:"/public/Images/Manipulation/OIP15.jpg",
+        viewLink:"https://drive.google.com/file/d/1EEQdIcmFmgsXO4nQvEJIejVmksxAfw07/view?usp=drive_link"
+    }
+   ,
+    {
+    
+        id:15,
+        title:"Wladislaw_Jachtchenko_The_Manipulation",
+        image:"/public/Images/Manipulation/download17.jpg",
+        viewLink:"https://drive.google.com/file/d/1IQhkHfxaMXKxVQnH3VxmsHspY-gvZlMy/view?usp=drive_link"
+    }
+   ,
+    {
+    
+        id:16,
+        title:"Wladislaw_Jachtchenko_The_Manipulation",
+        image:"/public/Images/Manipulation/download17.jpg",
+        viewLink:"https://drive.google.com/file/d/1IQhkHfxaMXKxVQnH3VxmsHspY-gvZlMy/view?usp=drive_link"
     }
 ]
 export default books
